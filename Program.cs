3e8fa3519cf5c43b2fde004e78840ad442ce320a
Dustin Walker <thewalkman1234@gmail.com>
2018-03-29 18:07:55 -0400

    while lopp init

--- Program.cs
+++ Program.cs
@@ -30,6 +30,38 @@ namespace numberguesser2
 
             Console.WriteLine("Hello {0}, Shall we play a game?", userName);
 
+            // Set Number
+            int correctNumber = 7;
+
+            // init Guess Var
+            int guessNumber = 0;
+
+            // Ask User For Number
+            Console.WriteLine("Guess a number between 1 and 10");
+
+            // while guess is not correct
+            while(guessNumber != correctNumber)
+            {
+                // Get Users Input
+                string userGuess = Console.ReadLine();
+
+                // Cast to int and put into guessNumber var
+                guessNumber = Int32.Parse(userGuess);
+
+                // Match Guess to Correct Number
+                if(guessNumber != correctNumber)
+                {
+                    // Change Text Color
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    // Write Out App Info
+                    Console.WriteLine("Wrong Number Please Try Again");
+
+                    Console.ResetColor();
+                }
+            }
+
+
         }
     }
 }
